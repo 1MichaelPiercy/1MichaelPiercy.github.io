80c03289541c982a40e6b1aa9d02268108238f79
1MichaelPiercy <piercymichael123@gmail.com>
2021-09-24 15:00:24 -0400

    copy

---
+++ itis3135/Scripts/first-scriptsjs.js
@@ -0,0 +1,74 @@
+
+var feelings = "";
+var myname = "";
+
+let companyName = "Machinelike Python";
+
+
+
+
+      myname = prompt("Can we get your name please?", "Alex Hope");   
+
+
+
+      feelings = prompt("How are you feeling?");
+
+
+ 
+
+
+    var day = new Date;
+    var hours = day.getHours() + ":" + day.getMinutes();
+
+    
+
+    window.alert("Today is " + day.toDateString() + ", and the time is: " + hours + "\n" + "The " + companyName + "welcomes you, " + 
+    myname + "!" + " We're glad you are doing " + feelings + "!");
+
+
+ 
+ 
+
+
+
+
+function makeAds1()
+{
+const makeAds = alert("Guess we gotta get these billboards up.");
+}
+
+
+function analyze()
+{
+    const analyzeSales = alert("Got to get them numbers up!");
+}
+
+ function improveSales()
+{
+     const improveSales = alert("Get them items out that door!");
+}
+
+function dMark()
+{
+    const marketing = alert("Enjoy the full mailbox!");
+}
+
+function design()
+{
+    const web = alert("Hope you got a good computer for these graphics!");
+}
+
+function stats()
+{
+    const boringMath = alert("Enjoy getting bored by the numbers!");
+}
+
+
+
+
+
+
+
+
+
+
