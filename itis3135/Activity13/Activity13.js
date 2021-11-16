9a7caaca7d1963e2cdaa09d465f5229c899a0d3d
1MichaelPiercy <piercymichael123@gmail.com>
2021-11-16 13:13:17 -0500

    update

--- itis3135/Activity13/Activity13.js
+++ itis3135/Activity13/Activity13.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
     $.ajax({
         type: "get",
-        url: "team.json",
+        url: "team.son",
         beforeSend: function() {
             $("#team").html("Loading...");
         },
@@ -9,7 +9,7 @@ $(document).ready(function() {
         error: function(xhr, status, error) {
             alert("Error: " + xhr.status + " - " + error);
         },
-        dataType: "json",
+        dataType: "xml",
         success: function(data) {
             $("#team").html("");
             $(data).find("management").children().each(function() {
@@ -20,5 +20,25 @@ $(document).ready(function() {
                           xmlDoc.find("bio").text() + "<br>");
             });
         }
+    });
+});*/
+
+$(document).ready(function() {
+    $.getJSON("team.json", function(data){
+
+        $.each(data, function(){
+
+          $.each(this, function(key , value){
+
+            $("#team").append(
+                "Name: " + value.name + "<br>" +
+                "Title: " + value.title + "<br>" +
+                "Bio: " + value.bio + "<br><br>"
+            )
+
+          });
+
+        });
+
     });
 });
