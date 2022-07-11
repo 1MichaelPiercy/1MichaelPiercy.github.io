a398feeff658c598cdf8d8c89287140ff8879e59
1MichaelPiercy <piercymichael123@gmail.com>
2022-07-10 21:55:40 -0400

    7/10/2022

---
+++ practice2/script.js
@@ -0,0 +1,6 @@
+//https://code.jquery.com/jquery-1.9.1.min.js
+//jquery/jquery-3.6.0.min.js
+
+$(document).ready(function() {
+    document.write("Hello, World! We are using jQuery from local machine");
+ });
